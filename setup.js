af4c757e08fc3d27cdfc6e6aaac27301cfb81cf3
Brian Schwartz <brianschwartz@northwesternmutual.com>
2019-05-09 13:19:15 -0500

    Allow port override

--- setup.js
+++ setup.js
@@ -2,19 +2,19 @@ const {resolve} = require('path');
 const cwd = require('cwd');
 const DynamoDB = require('aws-sdk/clients/dynamodb');
 const DynamoDbLocal = require('dynamodb-local');
+const {port} = require(resolve(cwd(), 'jest-dynamodb-config.js'));
 
 // aws-sdk requires access and secret key to be able to call DDB
 process.env.AWS_ACCESS_KEY_ID = 'access-key';
 process.env.AWS_SECRET_ACCESS_KEY = 'secret-key';
 
 const dynamoDB = new DynamoDB({
-  endpoint: 'localhost:8000',
+  endpoint: 'localhost:' + port || '8000',
   sslEnabled: false,
   region: 'local-env'
 });
 
 module.exports = async function() {
-  const {port} = require(resolve(cwd(), 'jest-dynamodb-config.js'));
   global.__DYNAMODB__ = await DynamoDbLocal.launch(port || 8000, null, ['-sharedDb']);
 
   await createTables();
