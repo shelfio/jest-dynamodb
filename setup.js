c77ec47f82f697b02d003863a9402bdd2580b533
Vlad Holubiev <golubev.vld@gmail.com>
2019-05-13 16:13:51 +0300

    refactor: use screaming snake case for constants

--- setup.js
+++ setup.js
@@ -2,11 +2,10 @@ const {resolve} = require('path');
 const cwd = require('cwd');
 const DynamoDB = require('aws-sdk/clients/dynamodb');
 const DynamoDbLocal = require('dynamodb-local');
-const defaultPort = 8000;
-let {port} = require(resolve(cwd(), 'jest-dynamodb-config.js'));
-if (typeof port === 'undefined' || port === null) {
-  port = defaultPort;
-}
+const config = require(resolve(cwd(), 'jest-dynamodb-config.js'));
+
+const DEFAULT_PORT = 8000;
+const port = typeof port === 'undefined' || port === null ? DEFAULT_PORT : config.port;
 
 // aws-sdk requires access and secret key to be able to call DDB
 process.env.AWS_ACCESS_KEY_ID = 'access-key';
