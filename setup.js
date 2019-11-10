943d0e9e38fe49aed612c4544633477d102ffd7a
Kris Thom White <kris.white@eightounce.com>
2019-11-10 10:45:28 -0600

    feat: add ability to pass dynamodb-local options from config file

--- setup.js
+++ setup.js
@@ -8,17 +8,18 @@ process.env.AWS_ACCESS_KEY_ID = 'access-key';
 process.env.AWS_SECRET_ACCESS_KEY = 'secret-key';
 
 const DEFAULT_PORT = 8000;
+const DEFAULT_OPTIONS = ['-sharedDb'];
 
 module.exports = async function() {
   const config = require(resolve(cwd(), 'jest-dynamodb-config.js'));
-  const {tables, port: port = DEFAULT_PORT} =
+  const {tables, port: port = DEFAULT_PORT, options: options = DEFAULT_OPTIONS} =
     typeof config === 'function' ? await config() : config;
   const dynamoDB = new DynamoDB({
     endpoint: 'localhost:' + port,
     sslEnabled: false,
     region: 'local-env'
   });
-  global.__DYNAMODB__ = await DynamoDbLocal.launch(port, null, ['-sharedDb']);
+  global.__DYNAMODB__ = await DynamoDbLocal.launch(port, null, options);
 
   await createTables(dynamoDB, tables);
 };
